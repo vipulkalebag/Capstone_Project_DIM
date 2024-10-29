55ddc6bf776ec94d4fd0cdeff47295cdde6f1bf3
vipulkalebag <vipulkalebag.1317@gmail.com>
2024-10-29 07:22:07 +0530

    Your commit message here

--- example-voting-app/healthchecks/postgres.sh
+++ example-voting-app/healthchecks/postgres.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 set -eo pipefail
 
-host="$(hostname -i || echo '127.0.0.1')"
-user="${POSTGRES_USER:-postgres}"
-db="${POSTGRES_DB:-$POSTGRES_USER}"
-export PGPASSWORD="${POSTGRES_PASSWORD:-}"
+# Define variables for the database connection
+host="$(hostname -i || echo '127.0.0.1')"  # Get the host IP address or default to localhost
+user="${POSTGRES_USER:-postgres}"          # Get the PostgreSQL user, default to 'postgres'
+db="${POSTGRES_DB:-$POSTGRES_USER}"        # Get the database name, default to the user name
+export PGPASSWORD="${POSTGRES_PASSWORD:-}"  # Set the password for PostgreSQL
 
-args=(
-	# force postgres to not use the local unix socket (test "external" connectibility)
-	--host "$host"
-	--username "$user"
-	--dbname "$db"
-	--quiet --no-align --tuples-only
-)
-
-if select="$(echo 'SELECT 1' | psql "${args[@]}")" && [ "$select" = '1' ]; then
-	exit 0
+# Run a simple query to check the connection
+if psql -h "$host" -U "$user" -d "$db" -c "SELECT 1;" --quiet --no-align --tuples-only; then
+    exit 0  # Success if the query returns 1
+else
+    exit 1  # Failure if the query does not return 1
 fi
 
-exit 1
--- example-voting-app/healthchecks/redis.sh
+++ example-voting-app/healthchecks/redis.sh
@@ -3,8 +3,8 @@ set -eo pipefail
 
 host="$(hostname -i || echo '127.0.0.1')"
 
-if ping="$(redis-cli -h "$host" ping)" && [ "$ping" = 'PONG' ]; then
-	exit 0
+if ping="$(redis-cli -h localhost ping)" && [ "$ping" = 'PONG' ]; then
+  exit 0
 fi
 
 exit 1
