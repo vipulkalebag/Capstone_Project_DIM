78b718d46626fd09a955de546fcf3a23e530c665
vipulkalebag <vipulkalebag.1317@gmail.com>
2024-11-15 13:22:18 +0530

    update all

--- example-voting-app/update_script/update-kube-spec.sh
+++ example-voting-app/update_script/update-kube-spec.sh
@@ -25,7 +25,7 @@ MICROSERVICES=("result" "vote" "worker")
 # Loop through each microservice and update the image tag in its Kubernetes manifest file
 for SERVICE in "${MICROSERVICES[@]}"; do
   # Construct the image tag using only the build number
-  # IMAGE_TAG="${BUILD_NUMBER}"  # Use only the build number as the image tag
+  IMAGE_TAG="${BUILD_NUMBER}"  # Use only the build number as the image tag
   # Get the ECR repo URL for the current service
   ECR_REPO_URL="${ECR_REPOS[$SERVICE]}"
   # [$SERVICE]
@@ -35,7 +35,7 @@ for SERVICE in "${MICROSERVICES[@]}"; do
   # DEPLOYMENT_FILE="Capstone_Project_DIM/example-voting-app/kube-spec/$SERVICE-deployment.yml"
 
   # Replace the image tag in the corresponding Kubernetes deployment file :$IMAGE_TAG
-  sed -i "s|image:.*|image: $ECR_REPO_URL|g" "./kube-spec/$SERVICE-deployment.yml"
+  sed -i "s|image:.*|image: $ECR_REPO_URL:$IMAGE_TAG|g" "./kube-spec/$SERVICE-deployment.yml"
   # if [ -f "$DEPLOYMENT_FILE" ]; then
   #   # Replace the image tag in the corresponding Kubernetes deployment file
   #   sed -i "s|image:.*|image: $ECR_REPO_URL/$SERVICE:$IMAGE_TAG|g" "$DEPLOYMENT_FILE"
